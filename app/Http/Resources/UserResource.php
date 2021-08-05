d05382e5f6516fdca4865b3c914d6f9e20d8cd95
Samuel Sarpong-Duah <sammav2018@gmail.com>
2021-08-05 13:09:56 +0000

    define display

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -14,6 +14,11 @@ class UserResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => (string)$this->id,
+            'username' => $this->username,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name
+        ];
     }
 }
